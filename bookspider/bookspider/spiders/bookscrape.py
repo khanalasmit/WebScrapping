d494eecf7d4e5289b1d29cd734d898733137104f
khanalasmit <asmitpasa2063@gmail.com>
2025-02-17 19:12:26 +0545

    corrected code

--- bookspider/bookspider/spiders/bookscrape.py
+++ bookspider/bookspider/spiders/bookscrape.py
@@ -1,26 +1,19 @@
 import scrapy
-
-
 class BookscrapeSpider(scrapy.Spider):
     name = "bookscrape"
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
-        books=response.css('article.product_pod')
+        books = response.css('article.product_pod')
         for book in books:
-            yield{
-                'name':book.css('h3 a::text').get(), 
-                'price':book.css('.product_price.price_color::text').get(),
-                'link':book.css('h3 a').attrib['href']
+            yield {
+                'name': book.css('h3 a::text').get(),
+                'price': book.css('.product_price .price_color::text').get(),
+                'link': response.urljoin(book.css('h3 a').attrib['href'])  # Absolute URL
             }
-        next_page=response.css('li.next a ::attr(href)').get()
-        if next_page is not None:
-            if 'catalouge/' in next_page:
-                
-                next_page_url='https:://books.toscrape.com/'+next_page
-            else:
-                next_page_url='https:://books.toscrape.com/catalogue/'+next_page
-                yield response.follow(next_page_url, callback=self.parse)
-                
-            
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page:
+            next_page_url = response.urljoin(next_page)  # This properly joins relative URLs
+            yield response.follow(url=next_page_url, callback=self.parse)
